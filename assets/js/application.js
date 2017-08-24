3a1a3664b54825650d19cb66df8b2375feb21742
Kyle Glick <glickk@musc.edu>
2017-08-24 17:43:01 -0400

    redo project accordion

--- assets/js/application.js
+++ assets/js/application.js
@@ -10,4 +10,14 @@ $(document).ready(function() {
   $(document).on('click', '.back-to-top', function() {
     $(this).blur()
   });
+
+  $(document).on('click', '.collapse-btn', function() {
+    if ($(this).attr('aria-expanded') == 'false') {
+      $(this).addClass('glyphicon-chevron-right')
+      $(this).removeClass('glyphicon-chevron-down')
+    } else {
+      $(this).removeClass('glyphicon-chevron-right')
+      $(this).addClass('glyphicon-chevron-down')
+    }
+  });
 });
