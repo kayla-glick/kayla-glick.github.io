e207507cd51080132ce680d2527683f955242769
Kyle Glick <glickk@musc.edu>
2017-09-26 21:11:54 -0400

    possible fix?

---
+++ _site/assets/js/application.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 50) {
+      $('.back-to-top').removeClass('hidden');
+    } else {
+      $('.back-to-top').addClass('hidden');
+    }
+  });
+
+  $(document).on('click', '.back-to-top', function() {
+    $(this).blur()
+  });
+
+  $(document).on('click', '.collapse-btn', function() {
+    if ($(this).attr('aria-expanded') == 'false') {
+      $(this).addClass('glyphicon-chevron-right')
+      $(this).removeClass('glyphicon-chevron-down')
+    } else {
+      $(this).removeClass('glyphicon-chevron-right')
+      $(this).addClass('glyphicon-chevron-down')
+    }
+  });
+
+  $(document).on('click', '.post img', function() {
+    $('#modal-place .modal-dialog').html($(this).clone());
+    $('#modal-place').modal('show');
+  });
+});
