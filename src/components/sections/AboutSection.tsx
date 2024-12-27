f1bc1f8bb385207a75488fec435923b7143f6616
Kayla Glick <12898988+kayla-glick@users.noreply.github.com>
2024-12-27 18:21:35 -0500

    Update AboutSection.tsx

--- src/components/sections/AboutSection.tsx
+++ src/components/sections/AboutSection.tsx
@@ -17,9 +17,11 @@ function AboutSection() {
               with 8 years of experience leading and collaborating with teams to design, build, & deploy pixel-perfect web applications at scale with a focus on UI/UX, performance, and observability.
             </p>
             <p>
-              I started my journey in college pursuing 3D modeling, environment art, & game development before jumping into the world ofweb development.
-              After making the switch, I quickly learned Ruby on Rails & had the opportunity to build tools used to aid medical research, making Cannabis more accessible
-              to people all over the world, & now I'm helping to shape the future of software at <a href="https://github.com" target="_blank" rel="noreferrer">GitHub</a>!
+              I started my journey in college pursuing 3D modeling, environment art, & game development before jumping into the world of web development.
+              After making the switch, I quickly learned Ruby on Rails & have since had the opportunity to build tools for managing medical research,
+              making Cannabis more accessible to people all over the world,
+              helping to shape the future of software at <a href="https://github.com" target="_blank" rel="noreferrer">GitHub</a>,
+              and now I'm part of the team at <a href="https://tern.travel" target="_blank" rel="noreferrer">Tern</a> working to build a better all-in-one platform for the Travel industry!
             </p>
             <p className="mb-0">Outside the office, you'll often find me:</p>
             <ul className="list-unstyled">
