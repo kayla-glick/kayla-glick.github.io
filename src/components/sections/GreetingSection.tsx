6ba26c140183d8a24e58701a38dfcfdaabb997c3
Kayla Glick <12898988+kayla-glick@users.noreply.github.com>
2024-12-27 18:18:32 -0500

    Update GreetingSection.tsx

--- src/components/sections/GreetingSection.tsx
+++ src/components/sections/GreetingSection.tsx
@@ -6,7 +6,7 @@ function GreetingSection() {
   return (
     <Section id="greeting">
       <h1 className="section-title mb-0">Kayla Glick</h1>
-      <h2 className="display-6 text-white mb-0">Senior Software Engineer @ GitHub</h2>
+      <h2 className="display-6 text-white mb-0">Senior Software Engineer @ Tern</h2>
       <p className="h5 text-white mb-4">Pixel-perfect websites for your picture-perfect business</p>
       <div className="d-flex align-items-center">
         <Button href="mailto:kaylaglick12@gmail.com" variant="outline-primary" size="lg" className="me-4">Get In Touch</Button>
